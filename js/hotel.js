694f919b529eac9f27c1d2725782ed4bbfd191b5
sebastianubaque <sebastian.15dd@gmail.com>
2018-09-24 10:12:03 -0500

    cambio 2

---
+++ js/hotel.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+// ------------------------bottom agregar--------------------------
+var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+
+
+
+  });
