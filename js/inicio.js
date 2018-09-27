1abe648b7213f7342bf1f16f0aa04286c7abde00
sebastianubaque <sebastian.15dd@gmail.com>
2018-09-27 07:54:17 -0500

    subiendo cambios

--- js/inicio.js
+++ js/inicio.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
 
+    var sidnav = document.querySelectorAll('.sidenav');
+    var instancessidnav = M.Sidenav.init(sidnav);
+
+    AOS.init({ once: true })
+
 
   });
