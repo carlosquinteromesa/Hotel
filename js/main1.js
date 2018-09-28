64c6abc038207b1228c91871e30e9182dd382f5b
carlosquinteromesa <carlosquin060400@gmail.com>
2018-09-28 11:44:35 -0500

    reservaciones

--- js/main1.js
+++ js/main1.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel')
-  
-   
-    var arrowright = document.querySelector('.arrowright')
-    var arrowleft = document.querySelector('.arrowleft')
-    var instance = M.Carousel.init(elems, {
-      fullWidth: true,
-      indicators: true
+  AOS.init();
+    let nav = document.querySelector('nav')
+    let elems = document.querySelectorAll('.scrollspy')
+    let options = {
+      scrollOffset: 100
+    }
+    var instances = M.ScrollSpy.init(elems, options)
+    window.addEventListener('scroll', () => {
+      if(window.pageYOffset>30){
+        nav.classList.add('nav-scroll')
+      }else if(window.pageYOffset<30){
+        nav.classList.remove('nav-scroll')
+      }
     })
-    
-    arrowright.addEventListener('click', () => {
-      var instances = M.Carousel.getInstance(elems[0])
-      instances.next()
-    })
-    arrowleft.addEventListener('click', () => {
-      var instances = M.Carousel.getInstance(elems[0])
-      instances.prev()
-    })
-   
-    
   })
